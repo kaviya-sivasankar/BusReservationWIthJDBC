459043041148221d10364aac4243b30344d47153
Kaviya S <kaviya.s0806@gmail.com>
2025-09-08 01:24:22 +0530

    Update DBConnection.java

--- com/busReservationWithJdbc/DBConnection.java
+++ com/busReservationWithJdbc/DBConnection.java
@@ -6,9 +6,9 @@ public class DBConnection {
 
 	private static final String url = "jdbc:mysql://localhost:3306/busreservation";
 	
-	private static final String userName = "root";
+	private static final String userName = "";
 	
-	private static final String password = "root";
+	private static final String password = "";
 	
 public static Connection getConnection() throws SQLException{
 		
